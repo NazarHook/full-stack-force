4343ba67808464d98c53283e072ac4c46a349248
BoikoAnton1992 <145497164+BoikoAnton1992@users.noreply.github.com>
2024-03-15 20:16:26 +0200

    fixed

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,7 +1,62 @@
 'use strict';
 import axios from 'axios';
 import Swiper from 'swiper';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import 'swiper/css';
-import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 console.log('hello');
+
+const BASE_URI = 'https://portfolio-js.b.goit.study/api/reviews';
+
+const reviews = await getReviewsFromServer();
+const galleryMarkup = createGalleryMarkup(reviews);
+
+document.getElementById('gallery').innerHTML = galleryMarkup;
+
+async function getReviewsFromServer() {
+  try {
+    const response = await axios.get(BASE_URI);
+    const data = response.data;
+    console.log('Received reviews from server:', data);
+    return data;
+  } catch (error) {
+    console.error('Error while fetching reviews:', error.message);
+    throw error;
+  }
+}
+
+function createGalleryMarkup(data) {
+  return `
+    <div class="swiper">
+        <p class="review-top">REVIEWS</p>
+      <div id = "swiper-wrapper" class="swiper-wrapper">
+        ${data
+          .map(
+            ({ author, avatar_url, review }) => `
+          <div class="swiper-slide">
+            <div class="gallery-item">
+              <img src="${avatar_url}" class="gallery-image" alt="${author}"/>
+                <div class="author">${author}</div>
+              <p class="review">${review}</p>
+            </div>
+          </div>
+          `
+          )
+          .join('')}
+            </div>
+            <div class= "swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+    </div>
+    `;
+}
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+  },
+  spaceBetween: 20,
+});
