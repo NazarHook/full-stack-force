757e1e104395d48ab5bfe3c44a2a1446fa8bc41c
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-03-15 20:03:39 +0200

    addded imports

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,6 +1,8 @@
 'use strict'
 import Swiper from 'swiper';
 import 'swiper/css';
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 console.log('hello');
