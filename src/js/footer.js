8179a2774165ab2057332e22e10e138bfb45d07a
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-03-18 01:32:09 +0200

    Update footer.js

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,1 +1,12 @@
 'use strict';
+const input = document.querySelector('.js-input');
+let maxInputNumber = input.maxLength;
+
+input.addEventListener('input', sliceText);
+const maxInputNumber = input.maxLength;
+
+function sliceText() {
+  if (input.value.length >= maxInputNumber) {
+    input.value = input.value.slice(0, maxInputNumber - 3) + '...';
+  }
+}
