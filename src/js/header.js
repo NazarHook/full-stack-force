c2347c8f0f868e0dc11cc8a91384fc14e558c962
Vika-Musiienko <vikamushinska@gmail.com>
2024-03-18 13:39:48 +0100

    header css

--- src/js/header.js
+++ src/js/header.js
@@ -1,14 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menu = document.querySelector('.menu');
-    const navLinks = document.querySelector('.nav ul');
- 
-    function toggleMenu() {
-      if (navLinks.style.display === 'none') {
-        navLinks.style.display = 'block';
-      } else {
-        navLinks.style.display = 'none';
-      }
+  const menu = document.querySelector('.menu');
+  const navLinks = document.querySelector('.nav ul');
+
+  function toggleMenu() {
+    if (navLinks.style.display === 'none') {
+      navLinks.style.display = 'block';
+    } else {
+      navLinks.style.display = 'none';
     }
-  
-    menu.addEventListener('click', toggleMenu);
+  }
+
+  menu.addEventListener('click', toggleMenu);
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const orderButton = document.querySelector('.button-active');
+
+  orderButton.addEventListener('click', function() {
+      const workTogetherSection = document.getElementById('work-together');
+      workTogetherSection.scrollIntoView({ behavior: 'smooth' });
   });
+});
