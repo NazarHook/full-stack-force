c1e9713d4d0f738b4373aa78b6c150f9cc02aa97
Vika-Musiienko <vikamushinska@gmail.com>
2024-03-18 15:51:45 +0100

    header new version

--- src/js/header.js
+++ src/js/header.js
@@ -1,25 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const menu = document.querySelector('.menu');
-  const navLinks = document.querySelector('.nav ul');
-
-  function toggleMenu() {
-    if (navLinks.style.display === 'none') {
-      navLinks.style.display = 'block';
-    } else {
-      navLinks.style.display = 'none';
-    }
+const menu = document.querySelector('.menu');
+  const navLinks = document.querySelectorAll('.nav ul li a');
+  function toggleLinks() {
+    navLinks.forEach(link => {
+      if (link.style.display === 'none') {
+        link.style.display = 'inline';
+      } else {
+        link.style.display = 'none';
+      }
+    });
   }
 
-  menu.addEventListener('click', toggleMenu);
-});
+  menu.addEventListener('click', toggleLinks);
 
 
+  document.addEventListener('DOMContentLoaded', function () {
+    const orderButton = document.querySelector('.button-active');
 
-document.addEventListener('DOMContentLoaded', function () {
-  const orderButton = document.querySelector('.button-active');
-
-  orderButton.addEventListener('click', function() {
-      const workTogetherSection = document.getElementById('work-together');
-      workTogetherSection.scrollIntoView({ behavior: 'smooth' });
-  });
+    orderButton.addEventListener('click', function() {
+        const workTogetherSection = document.getElementById('work-together');
+     workTogetherSection.scrollIntoView({ behavior: 'smooth' });
+    });
 });
