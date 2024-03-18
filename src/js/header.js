2dbe06df77d99a0d986518c1c9b674c8a7af00d0
Vika-Musiienko <vikamushinska@gmail.com>
2024-03-18 01:07:02 +0100

    header js+html

--- src/js/header.js
+++ src/js/header.js
@@ -1,2 +1,14 @@
-'use strict'
-console.log('hello');
+document.addEventListener('DOMContentLoaded', function () {
+    const menu = document.querySelector('.menu');
+    const navLinks = document.querySelector('.nav ul');
+ 
+    function toggleMenu() {
+      if (navLinks.style.display === 'none') {
+        navLinks.style.display = 'block';
+      } else {
+        navLinks.style.display = 'none';
+      }
+    }
+  
+    menu.addEventListener('click', toggleMenu);
+  });
