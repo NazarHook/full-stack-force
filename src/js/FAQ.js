4a497d27f54f9e523d4b3a69c11dae853bffd4e3
YehorGorishnii <131007854+YehorGorishnii@users.noreply.github.com>
2024-03-18 04:31:55 +0300

    1.0

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -1,4 +1,22 @@
-'use strict'
+'use strict';
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-console.log('hello');
+
+const accordions = Array.from(
+  document.querySelectorAll('.accordion-container')
+);
+new Accordion(accordions, {
+  showMultiple: true,
+});
+
+const accBtn = document.querySelectorAll('.more-btn');
+
+console.log(accBtn);
+
+for (let i = 0; i < accBtn.length; i++) {
+  accBtn[i].addEventListener('click', function () {
+    this.classList.toggle('active-btn');
+  });
+}
+
+
