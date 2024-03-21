c28145473bb594a4222ec3ce893b995e10f681b6
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-03-22 01:08:03 +0200

    Update FAQ.js

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -1,7 +1,6 @@
 'use strict';
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-
 const accordions = Array.from(
   document.querySelectorAll('.accordion-container')
 );
