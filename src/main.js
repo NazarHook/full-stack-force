6947733e2849b9d488df72e6a63db05e1c7e4794
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-03-22 21:46:21 +0000

    Deploying to gh-pages from @ NazarHook/full-stack-force@cf8f2f14fd9de00ab4226e9da2012fcaf5509d6a 🚀

--- src/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-import Swiper from 'swiper';
-import 'swiper/css';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import axios from 'axios';
